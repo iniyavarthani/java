2d79060fd90dbeb34f5c35612927779e3f1883c6
iniyavarthani <iniyamullai26@gmail.com>
2017-03-23 07:13:53 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,17 @@
+package array;
+
+public abstract class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]={10,11,1,13,6};
+//its an exception if we assign an valve of array than is size
+//System.out.println(a[1]);
+for(int i=0;i<a.length;i++)
+{
+	System.out.println(a[i]);
+}
+
+	}
+
+}
---
+++ ArraySec.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArraySec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]=new int[4];
+System.out.println("enter array values" );
+
+Scanner sc=new Scanner(System.in);
+a[0]=sc.nextInt();
+a[1]=sc.nextInt();
+a[2]=sc.nextInt();
+a[3]=sc.nextInt();
+
+
+
+
+	}
+	
+
+}
